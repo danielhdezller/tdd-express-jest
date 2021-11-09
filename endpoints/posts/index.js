c8936aa36ebeaa25c92bcb6b70d39daacabc5fd6
Daniel <dhdezllerena94@gmail.com>
2021-11-09 15:54:42 +0100

    feat(test): should create post

--- endpoints/posts/index.js
+++ endpoints/posts/index.js
@@ -1,5 +1,6 @@
 const postHandler = ({ axios }) => {
   get: () => {};
+  post: () => {};
 };
 
 module.exports = postHandler;
--- endpoints/posts/index.spec.js
+++ endpoints/posts/index.spec.js
@@ -1,7 +1,44 @@
-describe('Endpoints', () => {
-    describe('posts', () => {
-        it.skip('should create', () => {
-            
-        });
+const postHandlers = require("./index");
+
+describe("Endpoints", () => {
+  describe("posts", () => {
+    it.skip("should create", async () => {
+      //Arrange
+      const mockUsers = [
+        {
+          id: 1,
+        },
+        {
+          id: 2,
+        },
+      ];
+
+      const post = {
+        userId: 1,
+        id: 1,
+        title: "Title",
+        body: "The content of the post.",
+      };
+
+      const req = {
+        body: post,
+      };
+
+      const res = {
+        status: jest.fn(),
+        send: jest.fn,
+      };
+
+      const axios = {
+        get: jest.fn().mockResolvedValue({ data: mockUsers }),
+        post: jest.fn().mockResolvedValue({ data: {id: 1000}}),
+      };
+
+      //Act
+      await postHandlers({ axios }).post(req, res);
+
+      //Assert
+      expect(res.status.mock.calls).toEqual([[201]]);
     });
+  });
 });
